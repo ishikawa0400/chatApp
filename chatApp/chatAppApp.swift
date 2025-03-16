ddf3465f9d7bf7dfd152c03d55ffac079ecd6ec1
ishikawa0400 <ishikawa0400@gmail.com>
2025-03-16 20:01:34 +0900

    Initial Commit

---
+++ chatApp/chatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatAppApp.swift
+//  chatApp
+//
+//  Created by Ishikawa Yuki on 2025/03/16.
+//
+
+import SwiftUI
+
+@main
+struct chatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ chatAppTests/chatAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  chatAppTests.swift
+//  chatAppTests
+//
+//  Created by Ishikawa Yuki on 2025/03/16.
+//
+
+import Testing
+@testable import chatApp
+
+struct chatAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
